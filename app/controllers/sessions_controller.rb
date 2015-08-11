5d6619c2aa743962a677d341d5812f7fcd5711a7
Ankit Buddhiraju <activatedalien@gmail.com>
2015-08-11 09:27:26 -0700

    added authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:error] = @user.errors.full_messages
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ActiveRecord::Base
+  validates :username, :email, :password_digest, :session_token, presence: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  has_many :posts
+  has_many :events
+  has_many :forums
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def verify_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.verify_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
 end
